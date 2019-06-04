d46687ef28c8ce05a07bf3abd9a9ec1172932d45
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-04 22:58:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -4,9 +4,8 @@ def instantiate_new_array
 my_new_array = Array.new
 end
 
-def array_with_two_elements(my_two_array)
+def array_with_two_elements
 my_two_array = Array.new[2, "green"]
-  returns my_two_array[2, "green"]
 end
 
 
